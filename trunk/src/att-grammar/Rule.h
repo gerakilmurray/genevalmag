caaaf6b051a302c2d0b923537ab2f9449951b72e
gonzalopicco <gonzalopicco@89b21a92-930d-11de-afa2-6787fc62a48d>
2009-08-31 23:03:47 +0000

    def attr, rule, symbols->term and nonTerm

---
+++ trunk/src/att-grammar/Rule.h
@@ -0,0 +1,23 @@
+/**
+  * Rule.h
+  *
+  *  Created on: 31/08/2009
+  *  Authors:
+				  Kilmurray, Gerardo Luis.
+				  Picco, Gonzalo M.
+ */
+
+#ifndef RULE_H_
+#define RULE_H_
+
+namespace gen_eval_mag {
+
+class Rule {
+public:
+	Rule();
+	virtual ~Rule();
+};
+
+}
+
+#endif /* RULE_H_ */
---
+++ trunk/src/att-grammar/TerminalSymbol.cpp
@@ -0,0 +1,23 @@
+/**
+  * TerminalSymbol.cpp
+  *
+  *  Created on: 31/08/2009
+  *  Authors:
+				 Kilmurray, Gerardo Luis.
+                 Picco, Gonzalo M.
+ */
+
+#include "TerminalSymbol.h"
+
+namespace gen_eval_mag {
+
+TerminalSymbol::TerminalSymbol() {
+	// TODO Auto-generated constructor stub
+}
+
+
+TerminalSymbol::~TerminalSymbol() {
+	// TODO Auto-generated destructor stub
+}
+
+}
