e410544e47cfcbfdfcf6bf7b1287e302f79625ea
gonzalopicco <gonzalopicco@89b21a92-930d-11de-afa2-6787fc62a48d>
2009-09-25 21:25:53 +0000

    parse casi OK

--- GenEvalAG/src/Parser.cpp
+++ GenEvalAG/src/Parser.cpp
@@ -75,17 +75,20 @@ struct att_grammar: public grammar<att_grammar>
         {
             definition(att_grammar const &self)
             {
-            		r_ident = (+alpha_p) >> *(alnum_p);
-            		r_extends = strlit<>("extends:") >> r_ident[&v_extend] >> *(',' >> r_ident[&v_extend])>> ';';
-            		r_semantics = strlit<>("semantics:") >> r_ident[&v_semantic] >> *(',' >> r_ident[&v_semantic])>> ';';
-            		r_rules = strlit<>("rules:") >> r_ident[&v_rule] >> *(',' >> r_ident[&v_rule])>> ';';
-            		r_attributes = strlit<>("attributes:") >> r_ident[&v_attribute] >> *(',' >> r_ident[&v_attribute]) >> ';';
-            		at_grammar = !(r_extends) >> r_semantics >> r_attributes >> r_rules;
+            	r_ident = (alpha_p | ch_opt) >> *(alnum_p | ch_opt);
+
+            	r_semantics = strlit<>("semantics domains{") >> +bloq_sem >> strlit<>("}");
+            	bloq_sem = decl_op | decl_sort;
+            	decl_sort = strlit<>("sort ") >> r_ident >> *(',' >> r_ident) >> ';';
+            	decl_op = strlit<>("op ") >> !(mod_op >> '(' >> int_p >> ')') >>
+						  r_ident >> ':' >> dom_op >> strlit<>(":=>") >> r_ident  >> ';';
+            	dom_op = r_ident >> *(',' >> r_ident);
+            	mod_op = strlit<>("infix") | strlit<>("prefix") | strlit<>("sufix");
+            	ch_opt = ch_p ('+')|'_'|'*'|'/'|'^'|'%'|'&'|'<'|'='|'-'|'>';
             }
 
-            rule<ScannerT> r_ident, r_extends, r_semantics, r_rules, r_attributes, at_grammar;
-            rule<ScannerT> const&
-            start() const { return at_grammar; }
+            rule<ScannerT> r_ident, bloq_sem, r_semantics, decl_sort, decl_op, dom_op,mod_op,ch_opt;
+            rule<ScannerT> const& start() const { return r_semantics; }
         };
 };
 
@@ -101,33 +104,34 @@ bool parse_grammar(char const* str)
 ////////////////////////////////////////////////////////////////////////////
 int main()
 {
-//	FILE * pFile;
-//	char buffer[100];
+	FILE * pFile;
+	char buffer[100];
 	string texto;
 
-//	pFile = fopen ("/home/gonza/grammar.txt" , "r");
-//	if (pFile == NULL) perror ("Error opening file");
-//	else
-//	{
-//		while ( ! feof (pFile) )
-//		{
-//	          fgets (buffer , 100 , pFile);
-//	          texto += buffer;
-//	    }
-//		fclose (pFile);
-//	}
-//	string c;
+	pFile = fopen ("/home/gonza/TesisLic/repositorio/genevalmag/GenEvalAG/src/grammar.txt" , "r");
+	if (pFile == NULL) perror ("Error opening file");
+	else
+	{
+		while ( ! feof (pFile) )
+		{
+	          fgets (buffer , 100 , pFile);
+	          texto += buffer;
+	    }
+		fclose (pFile);
+	}
+	string c;
 
-	texto = "extends:standart,numeric,vergamota;semantics:f,g,d,pepito,lafuncion;attributes:a,valor,simb,b,nose;rules:r1,laregla,yosoynueva;";
-	cout << texto << endl;
+//	texto = "extends:standart,numeric,vergamota;semantics:f,g,d,pepito,lafuncion;attributes:a,valor,simb,b,nose;rules:r1,laregla,yosoynueva;";
+//	texto = "semantics domains{sort int;sort string,character;op mas:int,int->int;op sqr:int->int;sort pepe;}";
+//	cout << texto << endl;
 //	getline(cin, c);
 //	if (c=="q") return 0;
     if (parse_grammar(texto.c_str()))
     {
 		cout << "-------------------------\n";
 		cout << "Parsing succeeded\n";
-		cout << texto << " Parses OK: " << endl;
-		imprimir();
+		cout << texto << "\nParses OK: " << endl;
+		//imprimir();
 		cout << "-------------------------\n";
 	}
 	else
