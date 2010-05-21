d4fa39f4b061f02e9ad5cc5553acb19e7f6f438e
gerakilmurray <gerakilmurray@89b21a92-930d-11de-afa2-6787fc62a48d>
2010-05-21 23:45:05 +0000

    Arreglados los 3 ejemplos.

--- examples/ag_aritmetic/ag_aritmetic.cpp
+++ examples/ag_aritmetic/ag_aritmetic.cpp
@@ -1,6 +1,6 @@
 /**
-  *  \file      Eval_mag_proof.cpp
-  *  \brief     File of proof for Eval_mag. 
+  *  \file      ag_aritmetic.cpp
+  *  \brief     File of proof.
   *  \date      Thu Mar 25 21:04:46 2010
   *  \author    Kilmurray, Gerardo Luis <gerakilmurray@gmail.com>
   *  \author    Picco, Gonzalo Martin <gonzalopicco@gmail.com>
@@ -14,50 +14,12 @@
 using namespace evalmag;
 
 /**
-  * Main method of the Eval_mag.
+  * Main method.
   */
 int main()
 {
     maggen eval_mag;
-    eval_mag.print_v_seq();
-	eval_mag.translate_mag();
 
-/*    M root(1);
-    E node_1(2);
-    root.add(&node_1);
-    
-    E node_2(4);
-    E node_3(7);
-    node_1.add(&node_2).add(&node_3);
-    
-    E node_4(8);
-    E node_5(8);
-    node_2.add(&node_4).add(&node_5);
-    
-    E node_6(8);
-    node_3.add(&node_6);
-    
-    num node_7(11);
-    num node_8(9);
-    num node_9(10);
-    node_4.add(&node_7);
-    node_5.add(&node_8);
-    node_6.add(&node_9);
-    
-    digit node_11(16);
-    digit node_12(13);
-    num node_10(11);
-    real node_16(22);
-    node_7.add(&node_11);
-    node_8.add(&node_12).add(&node_10);
-    node_9.add(&node_16);
-
-    digit node_13(19);
-    digit node_14(14);
-    digit node_15(18);
-    node_10.add(&node_19);
-    node_16.add(&node_14).add(&node_15);
-*/    
     M root(1);
     E node_1(2);
     root.add(&node_1);
@@ -80,8 +42,6 @@ int main()
 
     cout << " After evaluation." << endl;
     cout << root.to_string() << endl;
-    
-    cout << "FIN EVAL_AG" << endl;
 
     return 0;
 }
--- examples/ag_count/ag_count.cpp
+++ examples/ag_count/ag_count.cpp
@@ -1,6 +1,6 @@
 /**
-  *  \file      Eval_mag_proof.cpp
-  *  \brief     File of proof for Eval_mag. 
+  *  \file      ag_count.cpp
+  *  \brief     File of proof.
   *  \date      Thu Mar 25 21:04:46 2010
   *  \author    Kilmurray, Gerardo Luis <gerakilmurray@gmail.com>
   *  \author    Picco, Gonzalo Martin <gonzalopicco@gmail.com>
@@ -14,28 +14,22 @@ using namespace std;
 using namespace evalmag;
 
 /**
-  * Main method of the Eval_mag.
+  * Main method.
   */
 int main()
 {
     maggen eval_mag;
-    eval_mag.print_v_seq();
-    eval_mag.translate_mag();
 
 // ROOT Extend
-
     S root(1);
 
 // ROOT
     T node_1(2);
-	
 	root.add(&node_1);
 	
 // childs ROOT
     T2 node_20(3);
-		
     T node_21(2);
-
     T node_22(4);
 
 	node_1.add(&node_20);
@@ -43,11 +37,8 @@ int main()
 	node_1.add(&node_22);
 	
 // childs of my second childs.
-	
     T2 node_210(3);
-		
     T node_211(4);
-
     T node_212(2);
 
 	node_21.add(&node_210);
@@ -55,11 +46,8 @@ int main()
 	node_21.add(&node_212);	
 
 // childs of thirtd child of my second child.
-	
     T2 node_2120(3);
-		
     T node_2121(4);
-
     T node_2122(4);
 
 	node_212.add(&node_2120);
@@ -70,8 +58,6 @@ int main()
 
     cout << " After evaluation." << endl;
     cout << root.to_string() << endl;
-    
-    cout << "FIN EVAL_AG" << endl;
 
     return 0;
 }
