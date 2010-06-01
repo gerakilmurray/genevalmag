ee9e82a157297c02bf35661983f2513014d3f116
gerakilmurray <gerakilmurray@89b21a92-930d-11de-afa2-6787fc62a48d>
2010-06-01 01:49:15 +0000

    agregados items

--- input_file_code/Node.hpp
+++ input_file_code/Node.hpp
@@ -19,18 +19,22 @@ namespace util_ag
 
 struct Node
 {
-  struct Node *parent;
-  vector<struct Node*> childs;
-  unsigned short rule_id;
-  unsigned short index_plan_v_seq; /* Index of vector de visit sequence. */
-  unsigned short num_v_seq;  /* Keeps the index of the current v_seq item. */
+  struct Node          *parent;
+  vector<struct Node*>  childs;
+  unsigned short        rule_id;
+  
+   /* Index of vector de visit sequence. */
+  unsigned short index_plan_v_seq;
+  
+  /* Keeps the index of the current v_seq item. */
+  unsigned short num_v_seq;
 
   virtual string to_string() const = 0;
   
   Node(const unsigned short &r_id)
   {
     rule_id = r_id;
-    parent = NULL;
+    parent  = NULL;
   }
   
   struct Node &add(struct Node *child)
