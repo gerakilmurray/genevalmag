0f22b3d10e86474b74ed18bebf14cb2835f38dfc
gerakilmurray <gerakilmurray@89b21a92-930d-11de-afa2-6787fc62a48d>
2010-06-02 15:27:28 +0000

    update los files input

--- branches/informe/input_file_code/Node.hpp
+++ branches/informe/input_file_code/Node.hpp
@@ -1,9 +1,9 @@
 /**
-  *  \file    Node.h
-  *  \brief   Header for node AST inside the MAG Evaluator.
-  *  \date    17/03/2010
-  *  \author  Kilmurray, Gerardo Luis <gerakilmurray@gmail.com>
-  *  \author  Picco, Gonzalo Martin <gonzalopicco@gmail.com>
+  *  \file      Node.h
+  *  \brief     Header for node AST inside the MAG Evaluator.
+  *  \date      17/03/2010
+  *  \author    Kilmurray, Gerardo Luis <gerakilmurray@gmail.com>
+  *  \author    Picco, Gonzalo Martin <gonzalopicco@gmail.com>
   */
 
 #ifndef NODE_HPP_
@@ -19,30 +19,30 @@ namespace util_ag
 
 struct Node
 {
-  struct Node          *parent;
-  vector<struct Node*>  childs;
-  unsigned short        rule_id;
-  
-   /* Index of vector de visit sequence. */
-  unsigned short index_plan_v_seq;
-  
-  /* Keeps the index of the current v_seq item. */
-  unsigned short num_v_seq;
+    struct Node *parent;
+    vector<struct Node*> childs;
+    unsigned short rule_id;
 
-  virtual string to_string() const = 0;
-  
-  Node(const unsigned short &r_id)
-  {
-    rule_id = r_id;
-    parent  = NULL;
-  }
-  
-  struct Node &add(struct Node *child)
-  {
-    childs.push_back(child);
-    child->parent = this;
-    return *this;
-  }
+    /* Index of vector de visit sequence. */
+    unsigned short index_plan_v_seq;
+
+    /* Keeps the index of the current v_seq item. */
+    unsigned short num_v_seq;
+
+    virtual string to_string() const = 0;
+    
+    Node(const unsigned short &r_id)
+    {
+        rule_id = r_id;
+        parent  = NULL;
+    }
+    
+    struct Node &add(struct Node *child)
+    {
+        childs.push_back(child);
+        child->parent = this;
+        return *this;
+    }
 };
 
 }/* end namespace */
