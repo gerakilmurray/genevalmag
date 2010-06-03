496c8984577165e10bf09999e784a598d49671b2
gerakilmurray <gerakilmurray@89b21a92-930d-11de-afa2-6787fc62a48d>
2010-06-03 22:23:20 +0000

    falta compute_eq

--- input_file_code/Node.hpp
+++ input_file_code/Node.hpp
@@ -1,9 +1,9 @@
 /**
-  *  \file      Node.h
-  *  \brief     Header for node AST inside the MAG Evaluator.
-  *  \date      17/03/2010
-  *  \author    Kilmurray, Gerardo Luis <gerakilmurray@gmail.com>
-  *  \author    Picco, Gonzalo Martin <gonzalopicco@gmail.com>
+  *  \file    Node.h
+  *  \brief   Header for node AST inside the MAG Evaluator.
+  *  \date    17/03/2010
+  *  \author  Kilmurray, Gerardo Luis <gerakilmurray@gmail.com>
+  *  \author  Picco, Gonzalo Martin <gonzalopicco@gmail.com>
   */
 
 #ifndef NODE_HPP_
@@ -20,21 +20,21 @@ namespace util_ag
 struct Node
 {
     struct Node *parent;
-    vector<struct Node*> childs;
+    vector <struct Node*> childs;
     unsigned short rule_id;
 
     /* Index of vector de visit sequence. */
-    unsigned short index_plan_v_seq;
+    unsigned short visit_seq_index;
 
-    /* Keeps the index of the current v_seq item. */
-    unsigned short num_v_seq;
+    /* Keeps the index of the current visit sequence item. */
+    unsigned short pos_visit_seq;
 
     virtual string to_string() const = 0;
     
     Node(const unsigned short &r_id)
     {
         rule_id = r_id;
-        parent  = NULL;
+        parent = NULL;
     }
     
     struct Node &add(struct Node *child)
