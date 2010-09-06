2ffa2ad0f7948c4ac929bc1147f1b0de3e6d8b4b
gerakilmurray <gerakilmurray@89b21a92-930d-11de-afa2-6787fc62a48d>
2010-09-06 13:58:48 +0000

    Agregada presentacion FINAL.

--- trunk/examples/ag_aritmetic/ag_aritmetic.cpp
+++ trunk/examples/ag_aritmetic/ag_aritmetic.cpp
@@ -25,18 +25,29 @@ int main()
     root.add(&node_1);
     
     E node_2(8);
-    E node_3(8);
+    E node_3(4);
     node_1.add(&node_2).add(&node_3);
+    
+    
+    E node_3_1(8);
+    E node_3_2(8);
+    node_3.add(&node_3_1).add(&node_3_2);
     
     num node_4(11);
     num node_5(11);
-    node_2.add(&node_4);
-    node_3.add(&node_5);
+    node_3_1.add(&node_4);
+    node_3_2.add(&node_5);
   
-    digit node_6(16);
-    digit node_7(19);
+    digit node_6(19);
+    digit node_7(14);
     node_4.add(&node_6);
     node_5.add(&node_7);
+    
+    num node_8(11);
+    node_2.add(&node_8);
+  
+    digit node_9(16);
+    node_8.add(&node_9);
     
     eval_mag.evaluator_mag(&root);
 
